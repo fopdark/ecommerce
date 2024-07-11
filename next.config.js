dac517950def4a4e31587eb55a1ec7746051d7bb
Henry Nguyen <fopdark@gmail.com>
2024-07-11 11:23:46 +0700

    update nextConfig

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
-const createNextIntlPlugin = require('next-intl/plugin');
- 
+const createNextIntlPlugin = require("next-intl/plugin");
+
 const withNextIntl = createNextIntlPlugin("./src/libs/i18n.ts");
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
+
 module.exports = withNextIntl(nextConfig);
