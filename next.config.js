1fac9973b13533c42a1ecc6fe45204a7514f88a6
Henry Nguyen <fopdark@gmail.com>
2024-07-11 10:10:15 +0700

    add new next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-// @ts-check
- 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    /* config options here */
-  }
-   
-  module.exports = nextConfig
+const withNextIntl = require('next-intl/plugin')('./src/libs/i18n.ts');
+
+module.exports = withNextIntl({});
